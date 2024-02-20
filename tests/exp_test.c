93d40f9bb3b59baeba048f2acc4feb7c05805c3a
Karim <kmouss8@teach-node-02.CS.McGill.CA>
2024-02-19 22:31:39 -0500

    modifited stmt test and added exp and sort linked list tests

---
+++ tests/exp_test.c
@@ -0,0 +1,39 @@
+
+
+int main()
+{
+
+    (x = a);
+    x = m + 1 * (n - 1);
+    a;
+    'c';
+    "asf";
+    '\t';
+    -1;
+    +d;
+    -a = a;
+    (t && f) < (a || f);
+    a > b;
+    a < b;
+    a >= b;
+    a <= b;
+    a == b;
+    a + b;
+    a - b;
+    a / b;
+    a *b;
+    a % b;
+    a || b;
+    a &&b;
+
+    &1;
+    *p;
+    &'c';
+    'c'.a;
+    'c'["afsaf"];
+    sizeof(void);
+    sizeof(struct b);
+    (int)c;
+
+    a(v, b, x[2], z[100][10], **p);
+}
--- tests/stmt_test.c
+++ tests/stmt_test.c
@@ -1,13 +1,59 @@
-#include "minic-stdlib.h"
 
-void main() {
-  int n;
-  int first;
-  int second;
-  int next;
-  int c;
-  char t;
 
-/
+int ia(int a[1], int b[2][30], int c[100][3][40], int d[1000])
+{
 
+  {
+  }
+
+  {
+    int a;
+    char b;
+    void c;
+    struct d l;
+    int* e;
+    char** f;
+    char*** g;
+    int h[10][2000][10];
+
+    while((x = y)) x+1;
+
+    if (x == 1){
+      x-1;
+      x*y;
+      z*f;
+      y/x;
+      v = v%2;
+      'a';
+      "adas";
+      z;
+      1;
+
+      return v;
+      return;
+      return c+s;
+
+      b[x+1];
+      d.x;
+      *x;
+      &x;
+      sizeof(int);
+      (int) x;
+      *(x+1);
+      &(x-1);
+
+      a(v,b,x[2],z[100][10],**p);
+
+
+
+    }else {
+
+      int a;
+
+      return;
+      continue;
+      break;
+      {}
+    }
+  }
 }
