15933b850ae6bac567141f5375826d0c0906c740
Karim Moussa <karim.moussa@mail.mcgill.ca>
2024-02-22 13:43:16 -0500

    added miniCStdLib functions to the AST program for autograder purposes

--- src/java/sem/SemanticAnalyzer.java
+++ src/java/sem/SemanticAnalyzer.java
@@ -3,16 +3,21 @@ package sem;
 import util.CompilerPass;
 
 public class SemanticAnalyzer extends CompilerPass {
-	
+
 	public void analyze(ast.Program prog) {
+
+		prog.addMiniCStdLib();
 
 		NameAnalyzer na = new NameAnalyzer();
 		na.visit(prog);
 		this.numErrors += na.getNumErrors();
 
+		FunProtoAnalyzer fpa = new FunProtoAnalyzer();
+		fpa.visit(prog);
+		this.numErrors += fpa.getNumErrors();
+
 		TypeAnalyzer tc = new TypeAnalyzer();
 		tc.visit(prog);
 		this.numErrors += tc.getNumErrors();
-		// To complete
 	}
 }
