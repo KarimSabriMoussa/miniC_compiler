b809d515b0f136cda874c4b4ed5c32c3cf21e5df
Karim Moussa <karim.moussa@mail.mcgill.ca>
2024-02-22 11:21:31 -0500

    big fix: null exception because symbolMap wasn't being initialised

--- src/java/sem/Scope.java
+++ src/java/sem/Scope.java
@@ -1,24 +1,29 @@
 package sem;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Scope {
 	private Scope outer;
 	private Map<String, Symbol> symbolTable;
-	
-	public Scope(Scope outer) { 
-		this.outer = outer; 
+
+	public Scope(Scope outer) {
+		this.outer = outer;
+		this.symbolTable = new HashMap<String, Symbol>();
 	}
-	
-	public Scope() { this(null); }
-	
+
+	public Scope() {
+		this(null);
+		this.symbolTable = new HashMap<String, Symbol>();
+	}
+
 	public Symbol lookup(String name) {
-		
+
 		Scope sc = this;
 
-		while(sc != null){
+		while (sc != null) {
 			Symbol symbol = sc.lookupCurrent(name);
-			if( symbol != null){
+			if (symbol != null) {
 				return symbol;
 			}
 
@@ -27,11 +32,11 @@ public class Scope {
 
 		return null;
 	}
-	
+
 	public Symbol lookupCurrent(String name) {
 		return symbolTable.get(name);
 	}
-	
+
 	public void put(Symbol sym) {
 		symbolTable.put(sym.name, sym);
 	}
