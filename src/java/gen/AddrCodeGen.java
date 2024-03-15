e23d9893edc1daf2014619280ee68b6139f3296d
Karim Moussa <karim.moussa@mail.mcgill.ca>
2024-03-15 12:10:04 -0400

    implemented codegen for expressions and producing addresses

--- src/java/gen/AddrCodeGen.java
+++ src/java/gen/AddrCodeGen.java
@@ -1,11 +1,12 @@
 package gen;
 
-import ast.Expr;
-import gen.asm.AssemblyProgram;
-import gen.asm.Register;
+import ast.*;
+import gen.asm.*;
+import gen.asm.AssemblyProgram.*;
 
 /**
- * Generates code to calculate the address of an expression and return the result in a register.
+ * Generates code to calculate the address of an expression and return the
+ * result in a register.
  */
 public class AddrCodeGen extends CodeGen {
 
@@ -14,8 +15,52 @@ public class AddrCodeGen extends CodeGen {
     }
 
     public Register visit(Expr e) {
-        // TODO: to complete
-        return null;
+        return switch (e) {
+            case VarExpr ve -> {
+                Section currSection = asmProg.getCurrentSection();
+                Register addr = Register.Virtual.create();
+
+                if (ve.vd.globalLabel != null) {
+                    currSection.emit(OpCode.LA, addr, ve.vd.globalLabel);
+                } else {
+                    currSection.emit(OpCode.ADDIU, addr, Register.Arch.fp, ve.vd.fpOffset);
+                }
+
+                yield addr;
+            }
+            case FieldAccessExpr fae -> {
+
+                Section currSection = asmProg.getCurrentSection();
+                Register addr = visit(fae.struct);
+                VarDecl member = fae.getMember(fae.member);
+
+                currSection.emit(OpCode.ADDIU, addr, addr, member.structOffset);
+
+                yield addr;
+            }
+            case ArrayAccessExpr aae -> {
+                Section currSection = asmProg.getCurrentSection();
+                Register addr = Register.Virtual.create();
+
+                Register baseAddr = visit(aae.array);
+                Register offset = (new ExprCodeGen(asmProg)).visit(aae.index);
+                Register typeSize = Register.Virtual.create();
+                currSection.emit(OpCode.LI, typeSize, Type.getSize(aae.type));
+                currSection.emit(OpCode.MUL, offset, offset, typeSize);
+                currSection.emit(OpCode.ADD, addr, baseAddr, offset);
+                yield addr;
+            }
+            case ValueAtExpr vae -> {
+                Section currSection = asmProg.getCurrentSection();
+                Register addr = visit(vae.expr);
+                Register addrPointedTo = Register.Virtual.create();
+                currSection.emit(OpCode.LW, addrPointedTo, addr, 0);
+                yield addrPointedTo;
+            }
+            default -> {
+                yield null;
+            }
+        };
     }
 
 }
--- src/java/gen/ExprCodeGen.java
+++ src/java/gen/ExprCodeGen.java
@@ -1,16 +1,17 @@
 package gen;
 
 import ast.*;
-import gen.asm.AssemblyProgram;
-import gen.asm.OpCode;
-import gen.asm.Register;
-import gen.asm.Register.Arch;
-import gen.asm.AssemblyProgram.Section;
+import gen.asm.*;
+import gen.asm.AssemblyProgram.*;
+import java.util.Set;
 
 /**
  * Generates code to evaluate an expression and return the result in a register.
  */
 public class ExprCodeGen extends CodeGen {
+
+    private Set<String> minicStdLibFunctions = Set.of("print_i", "print_c", "print_s", "read_i", "read_c",
+            "mcmalloc");
 
     public ExprCodeGen(AssemblyProgram asmProg) {
         this.asmProg = asmProg;
@@ -19,18 +20,164 @@ public class ExprCodeGen extends CodeGen {
     public Register visit(Expr e) {
 
         return switch (e) {
+            case AddressOfExpr aoe -> {
 
+                Register addr = (new AddrCodeGen(asmProg)).visit(aoe.expr);
+                yield addr;
+            }
+            case Assign a -> {
+
+                Register addr = (new AddrCodeGen(asmProg)).visit(a.variable);
+                Register value = visit(a.value);
+
+                Section currSection = asmProg.getCurrentSection();
+
+                switch (a.variable.type) {
+                    case BaseType.INT -> {
+                        currSection.emit(OpCode.SW, value, addr, 0);
+                    }
+                    case BaseType.CHAR -> {
+                        currSection.emit(OpCode.SB, value, addr, 0);
+                    }
+                    case StructType st1 -> {
+                        switch (a.value.type) {
+                            case StructType st2 -> {
+                                for (int i = 0; i < st1.std.size; i += 4) {
+                                    Register word = Register.Virtual.create();
+                                    currSection.emit(OpCode.LW, word, value, i);
+                                    currSection.emit(OpCode.SW, word, addr, i);
+                                }
+                            }
+                            default -> {
+
+                            }
+                        }
+                    }
+                    case PointerType pt -> {
+                        currSection.emit(OpCode.SW, value, addr, 0);
+                    }
+                    default -> {
+
+                    }
+                }
+
+                yield value;
+            }
+            case ArrayAccessExpr aae -> {
+                Section currSection = asmProg.getCurrentSection();
+                Register addr = (new AddrCodeGen(asmProg)).visit(aae);
+                Register result = Register.Virtual.create();
+
+                switch (aae.type) {
+                    case BaseType.INT -> {
+                        currSection.emit(OpCode.LW, result, addr, 0);
+                    }
+                    case BaseType.CHAR -> {
+                        currSection.emit(OpCode.LB, result, addr, 0);
+                    }
+                    case ArrayType at -> {
+                        yield addr;
+                    }
+                    case PointerType pt -> {
+                        currSection.emit(OpCode.LW, result, addr, 0);
+                    }
+                    default -> {
+
+                    }
+                }
+
+                yield result;
+            }
+            case FieldAccessExpr fae -> {
+                Section currSection = asmProg.getCurrentSection();
+                Register result = Register.Virtual.create();
+                Register addr = (new AddrCodeGen(asmProg)).visit(fae);
+
+                VarDecl member = fae.getMember(fae.member);
+
+                switch (member.type) {
+                    case BaseType.INT -> {
+                        currSection.emit(OpCode.LW, result, addr, 0);
+                    }
+                    case BaseType.CHAR -> {
+                        currSection.emit(OpCode.LB, result, addr, 0);
+                    }
+                    case PointerType pt -> {
+                        currSection.emit(OpCode.LW, result, addr, 0);
+                    }
+                    case ArrayType at -> {
+                        currSection.emit(OpCode.ADDIU, result, addr, 0);
+                    }
+                    case StructType st -> {
+                        currSection.emit(OpCode.ADDIU, result, addr, 0);
+                    }
+                    default -> {
+
+                    }
+                }
+
+                yield result;
+
+            }
+            case SizeOfExpr soe -> {
+                Section currSection = asmProg.getCurrentSection();
+                Register resultRegister = Register.Virtual.create();
+                currSection.emit(OpCode.LI, resultRegister, Type.getSize(soe.target_type));
+
+                yield resultRegister;
+            }
             case BinOp bo -> {
                 Section currSection = asmProg.getCurrentSection();
-                Register leftRegister = visit(bo.leftOperand);
-                Register rightRegister = visit(bo.rightOperand);
+                Register leftRegister;
+                Register rightRegister;
                 Register resultRegister = Register.Virtual.create();
+
+                switch (bo.op) {
+                    case AND:
+                        Label falseLabel = Label.create();
+                        Label endAND = Label.create();
+
+                        leftRegister = visit(bo.leftOperand);
+                        currSection.emit(OpCode.BEQZ, leftRegister, falseLabel);
+
+                        rightRegister = visit(bo.rightOperand);
+                        currSection.emit(OpCode.ADDIU, resultRegister, rightRegister, 0);
+                        currSection.emit(OpCode.B, endAND);
+
+                        currSection.emit(falseLabel);
+                        currSection.emit(OpCode.LI, resultRegister, 0);
+
+                        currSection.emit(endAND);
+
+                        yield resultRegister;
+                    case OR:
+                        Label trueLabel = Label.create();
+                        Label endOR = Label.create();
+
+                        leftRegister = visit(bo.leftOperand);
+                        currSection.emit(OpCode.BNEZ, leftRegister, trueLabel);
+
+                        rightRegister = visit(bo.rightOperand);
+                        currSection.emit(OpCode.BNEZ, rightRegister, trueLabel);
+                        currSection.emit(OpCode.LI, resultRegister, 0);
+                        currSection.emit(OpCode.B, endOR);
+
+                        currSection.emit(trueLabel);
+                        currSection.emit(OpCode.LI, resultRegister, 1);
+
+                        currSection.emit(endOR);
+
+                        yield resultRegister;
+                    default:
+                        break;
+                }
+
+                leftRegister = visit(bo.leftOperand);
+                rightRegister = visit(bo.rightOperand);
+
                 switch (bo.op) {
                     case ADD:
                         currSection.emit(OpCode.ADD, resultRegister, leftRegister, rightRegister);
-                        break;
-                    case AND:
-                        currSection.emit(OpCode.AND, resultRegister, leftRegister, rightRegister);
                         break;
                     case DIV:
                         currSection.emit(OpCode.DIV, leftRegister, rightRegister);
@@ -65,9 +212,6 @@ public class ExprCodeGen extends CodeGen {
                         currSection.emit(OpCode.XOR, resultRegister, leftRegister, rightRegister);
                         currSection.emit(OpCode.SLTU, resultRegister, Register.Arch.zero, resultRegister);
                         break;
-                    case OR:
-                        currSection.emit(OpCode.OR, resultRegister, leftRegister, rightRegister);
-                        break;
                     case SUB:
                         currSection.emit(OpCode.SUB, resultRegister, leftRegister, rightRegister);
                         break;
@@ -78,30 +222,117 @@ public class ExprCodeGen extends CodeGen {
                 yield resultRegister;
             }
             case FunCallExpr fce -> {
-                switch (fce.name) {
-                    case "print_i":
-                        Section currSection = asmProg.getCurrentSection();
-                        currSection.emit(OpCode.LI, Arch.v0, 1);
-                        currSection.emit(OpCode.ADDI, Arch.a0, Arch.zero, ((IntLiteral) fce.args.get(0)).value);
-                        currSection.emit(OpCode.SYSCALL);
-                    default:
-                        break;
+                if (minicStdLibFunctions.contains(fce.name)) {
+                    yield (new MinicStdLibFunctionGen(asmProg)).visit(fce);
                 }
 
-                yield null;
+                Section currSection = asmProg.getCurrentSection();
+                Register returnValue = Register.Virtual.create();
+
+                currSection.emit(OpCode.ADDIU, Register.Arch.sp, Register.Arch.sp, -fce.fd.argSegmentSize);
+
+                for (int i = 0; i < fce.args.size(); i++) {
+                    Register argValue = visit(fce.args.get(i));
+                    VarDecl param = fce.fd.params.get(i);
+                    switch (param.type) {
+                        case BaseType.INT -> {
+                            currSection.emit(OpCode.SW, argValue, Register.Arch.sp, param.fpOffset);
+                        }
+                        case BaseType.CHAR -> {
+                            currSection.emit(OpCode.SB, argValue, Register.Arch.sp, param.fpOffset);
+                        }
+                        case PointerType pt -> {
+                            currSection.emit(OpCode.SW, argValue, Register.Arch.sp, param.fpOffset);
+                        }
+                        case ArrayType at -> {
+                            currSection.emit(OpCode.SW, argValue, Register.Arch.sp, param.fpOffset);
+                        }
+                        case StructType st -> {
+                            for (int j = 0; j < st.std.size; j += 4) {
+                                Register word = Register.Virtual.create();
+                                currSection.emit(OpCode.LW, word, argValue, j);
+                                currSection.emit(OpCode.SW, word, Register.Arch.sp, param.fpOffset + j);
+                            }
+                        }
+                        default -> {
+
+                        }
+                    }
+                }
+
+                currSection.emit(OpCode.JAL, fce.fd.label);
+
+                switch (fce.fd.type) {
+                    case BaseType.INT -> {
+                        currSection.emit(OpCode.LW, returnValue, Register.Arch.sp, 0);
+                    }
+                    case BaseType.CHAR -> {
+                        currSection.emit(OpCode.LB, returnValue, Register.Arch.sp, 0);
+                    }
+                    case ArrayType at -> {
+                        currSection.emit(OpCode.LW, returnValue, Register.Arch.sp, 0);
+                    }
+                    case PointerType pt -> {
+                        currSection.emit(OpCode.LW, returnValue, Register.Arch.sp, 0);
+                    }
+                    case StructType st -> {
+                        currSection.emit(OpCode.ADDIU, returnValue, Register.Arch.sp, 0);
+                    }
+                    default -> {
+
+                    }
+                }
+
+                currSection.emit(OpCode.ADDIU, Register.Arch.sp, Register.Arch.sp, fce.fd.argSegmentSize);
+
+                yield returnValue;
             }
             case VarExpr ve -> {
-                if (ve.vd.globalLabel != null) {
-                    Section currSection = asmProg.getCurrentSection();
-                    Register addr = Register.Virtual.create();
-                    Register dst = Register.Virtual.create();
-                    currSection.emit(OpCode.LA, addr, ve.vd.globalLabel);
-                    currSection.emit(OpCode.LW, dst, addr, 0);
+                Section currSection = asmProg.getCurrentSection();
 
-                    yield dst;
+                Register addr = (new AddrCodeGen(asmProg)).visit(ve);
+                Register value = Register.Virtual.create();
+                switch (ve.vd.type) {
+                    case BaseType.INT -> {
+                        currSection.emit(OpCode.LW, value, addr, 0);
+                    }
+                    case BaseType.CHAR -> {
+                        currSection.emit(OpCode.LB, value, addr, 0);
+                    }
+                    case ArrayType at -> {
+                        if (ve.vd.fpOffset > 0) {
+                            Register arrayAddr  = Register.Virtual.create();
+                            currSection.emit(OpCode.LW, arrayAddr, addr, 0);
+                            yield arrayAddr;
+                        } else {
+                            yield addr;
+                        }
+                    }
+                    case PointerType pt -> {
+                        currSection.emit(OpCode.LW, value, addr, 0);
+                    }
+                    case StructType st -> {
+                        yield addr;
+                    }
+                    default -> {
+
+                    }
                 }
 
-                yield null;
+                yield value;
+
+            }
+            case ChrLiteral cl -> {
+                Section currSection = asmProg.getCurrentSection();
+                Register dst = Register.Virtual.create();
+                currSection.emit(OpCode.LI, dst, cl.chr);
+                yield dst;
+            }
+            case IntLiteral il -> {
+                Section currSection = asmProg.getCurrentSection();
+                Register dst = Register.Virtual.create();
+                currSection.emit(OpCode.LI, dst, il.value);
+                yield dst;
             }
             case StrLiteral sl -> {
                 Section currSection = asmProg.getCurrentSection();
@@ -109,10 +340,42 @@ public class ExprCodeGen extends CodeGen {
                 currSection.emit(OpCode.LA, addr, sl.label);
                 yield addr;
             }
-            default -> {
-
-                yield null;
+            case TypecastExpr tce -> {
+                Register resultRegister = visit(tce.expr);
+                yield resultRegister;
             }
+            case ValueAtExpr vae -> {
+
+                Section currSection = asmProg.getCurrentSection();
+                Register addrPointedTo = (new AddrCodeGen(asmProg)).visit(vae);
+                Register result = Register.Virtual.create();
+
+                switch (vae.type) {
+                    case BaseType.CHAR -> {
+                        currSection.emit(OpCode.LB, result, addrPointedTo, 0);
+                    }
+                    case BaseType.INT -> {
+                        currSection.emit(OpCode.LW, result, addrPointedTo, 0);
+                    }
+                    case PointerType pt -> {
+                        currSection.emit(OpCode.LW, result, addrPointedTo, 0);
+                    }
+                    case ArrayType at -> {
+                        currSection.emit(OpCode.LW, result, addrPointedTo, 0);
+                    }
+                    case StructType st -> {
+                        currSection.emit(OpCode.LW, result, addrPointedTo, 0);
+                    }
+                    default -> {
+
+                    }
+                }
+
+                yield result;
+
+            }
+
         };
     }
+
 }
